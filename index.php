ff2dc91a2655fd869ff57bb42f86396079e748dc
arnau <arnau@DESKTOP-4NC1U8A>
2018-11-15 12:19:07 +0100

    -Carpeta assets para alojar CSS, Fuentes y imagenes -Favicon.ico -Creacion SOLO Front-End de la pagina de inicio que es el Login, el diseño no es estructural sino que es el "Currado" echo para tener y mostrar las pautas de diseño que seguiremos, por ejemplo los colores, las fuentes y los px de los bordes y asi seguir el resto de la web con ese diseño.

--- index.php
+++ index.php
@@ -1,18 +1,69 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        echo "test";
-        ?>
-    </body>
+<html lang="es">
+<head>
+	<title>AOM Subscription Manager</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/css/inicio-sesion/style.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<!--===============================================================================================-->
+</head>
+<body>
+<div class="limitador">
+		<div class="todo">
+			<div class="caja">	
+			<form class="formulario" action="" method="post" >
+					<span class="titulo">AOM</span> 
+					<span class="sub-titulo">Versión 0.0</span>
+				 <span class="sub-titulo"></span>
+					<div class="input" >
+						<input class="caja-input" type="text" id="email" name="email" autocomplete="new-password" placeholder="Email"  required>
+						<span class="icono">
+							<i class="fa fa-user-circle" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="input" >
+						<input class="caja-input" type="password" id="password" name="password" autocomplete="new-password" placeholder="Contraseña" required>						
+						<span class="icono">
+						<i class="fas fa-lock-open"></i>
+						</span>
+					</div>
+				
+					
+					<div class="todo-form-btn">
+						<button type="submit" class="caja-titulo-btn">
+					ENTRAR
+						</button>
+					</div><br>
+				
+				<?php
+			
+				?>
+
+
+					<div class="text-center">
+						<a class="txt5" href="recuperar-password/">
+						 Recuperar contraseña  <i class="fa fa-key" aria-hidden="true"></i>
+						  </a><br>
+						  <a class="txt4" href="registre/">
+						  Registro <i class="fas fa-user-plus"></i>
+						  </a><br><br>
+					
+						</div>
+				</form>
+			</div>
+		</div>
+		<div id="footerRedesSocialesPermanente">
+			<div class="flotanteIzquierdaFooter">
+				<a >AOM Team</a>
+			</div>
+			<div class="flotanteDerechaFooter">
+				<a href="#"> © Copyright 2018</a>
+			</div>
+		</div>
+</body>
 </html>
