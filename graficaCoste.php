3527e81f54e5549031af0c30dc91ee4ec61172d9
marcroigchueca <marcroigchueca@localhost>
2019-01-11 09:02:33 +0100

    GRAFICA INTERACTIVA A TIEMPO REAL EN PROCESO

--- graficaCoste.php
+++ graficaCoste.php
@@ -1,18 +1,85 @@
-<?php
-require_once("./phpChart_Lite/conf.php");
-?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>phpChart - A Basic Chart</title>
-</head>
-<body>
-    
-<?php
-$pc = new C_PhpChartX(array(array(11, 9, 5, 12, 14)),'basic_chart');
-$pc->draw();
-?>
+<HTML>
+<BODY>
 
-</body>
+<meta charset="utf-8"> 
+
+<?php
+require_once("randomClass.php");
+
+//Creamos un objeto de la clase randomTable
+$rand = new RandomTable();
+//insertamos un valor aleatorio
+
+//obtenemos toda la información de la tabla random
+$rawdata = $rand->getAllInfo();
+print_r($rawdata);
+//nos creamos dos arrays para almacenar el tiempo y el valor numérico
+$valoresArray;
+$timeArray;
+//en un bucle for obtenemos en cada iteración el valor númerico y 
+//el TIMESTAMP del tiempo y lo almacenamos en los arrays 
+for($i = 0 ;$i<count($rawdata);$i++){
+    $valoresArray[$i]= $rawdata[$i][1];
+    //OBTENEMOS EL TIMESTAMP
+    $time= $rawdata[$i][2];
+    $date = new DateTime($time);
+    //ALMACENAMOS EL TIMESTAMP EN EL ARRAY
+    $timeArray[$i] = $date->getTimestamp()*1000;
+}
+
+?>
+<div id="contenedor"></div>
+
+<script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Importo el archivo Javascript de Highcharts directamente desde su servidor -->
+<script src="http://code.highcharts.com/stock/highstock.js"></script>
+<script src="http://code.highcharts.com/modules/exporting.js"></script>
+<script>
+
+chartCPU = new Highcharts.StockChart({
+    chart: {
+        renderTo: 'contenedor'
+        //defaultSeriesType: 'spline'
+
+    },
+    rangeSelector : {
+        enabled: false
+    },
+    title: {
+        text: 'Gráfica'
+    },
+    xAxis: {
+        type: 'datetime'
+        //tickPixelInterval: 150,
+        //maxZoom: 20 * 1000
+    },
+    yAxis: {
+        minPadding: 0.2,
+        maxPadding: 0.2,
+        title: {
+            text: 'Valores',
+            margin: 10
+        }
+    },
+    series: [{
+        name: 'valor',
+        data: (function() {
+                // generate an array of random data
+                var data = [];
+                <?php
+                    for($i = 0 ;$i<count($rawdata);$i++){
+                ?>
+                data.push([<?php echo $timeArray[$i];?>,<?php echo $valoresArray[$i];?>]);
+                <?php } ?>
+                return data;
+            })()
+    }],
+    credits: {
+            enabled: false
+    }
+});
+
+</script>   
+</BODY>
+
 </html>
