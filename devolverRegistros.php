69d5f8105fc20f4e6920c5efa6177759e5a5fe27
marcroigchueca <marcroigchueca@localhost>
2019-01-30 12:21:33 +0100

    FUNCION SENDMAIL() hecha

--- devolverRegistros.php
+++ devolverRegistros.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $server = "localhost";
 $user = "root";
             $password = "";
@@ -20,7 +21,13 @@ if ($resul->rowCount() > 0) {
 
     while ($row = $resul->fetch()) {
         
-        sendMail();
+        print_r($row);
+        $email=$row['email'];
+        $subject="Renovación..-";
+        $body="Hola ".$row['firstName']." tu suscripción de ".$row['subscriptionName']."te caduca en ".$row['remainMe']." dias, recuerda renovar la sucripción."
+                . "Gracias."
+                . "Atentamente AOM.";
+        sendMail($email, $subject, $body);
 
       
 
@@ -58,39 +65,38 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 
            
+            
+
+            
+            function sendMail($email,$subject, $body){
+                
             require 'phpmailer/src/Exception.php';
             require 'phpmailer/src/PHPMailer.php';
             require 'phpmailer/src/SMTP.php';
-
-            
-            function sendMail(){
-                
-            
             $mail = new PHPMailer;
 
 
 
-            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->isSMTP();  
+            // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = 'aomsubscriptionmanager@gmail.com';                 // SMTP username
             $mail->Password = 'Barcelona12345!';                           // SMTP password
             $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 587;                                    // TCP port to connect to
+            $mail->Port = 587;   
+            $mail->charSet="UTF-8";// TCP port to connect to
 
-            $mail->setFrom($email, "$nombre $apellido");
-            $mail->addAddress('aomsubscriptionmanager@gmail.com');     // Add a recipient
-            $mail->addReplyTo($email, 'Information');
-
+             $mail->setFrom('aomsubscriptionmanager@gmail.com', "AOM");
+            $mail->addAddress($email);     // Add a recipient
+//                $mail->addAddress('ellen@example.com');
 
 //                $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //                $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
             $mail->isHTML(true);                                  // Set email format to HTML
 
-            $mail->Subject = 'RECORDATORIO PAGO SUBCRIPCIÓN';
-            $mail->Body = "Nombre: $nombre <br> Teléfono: $telephone <br>Consulta: $message <br>Email del usuario: $email ";
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+            $mail->Subject = $subject;
+            $mail->Body = $body;
             if (!$mail->send()) {
                 echo 'Mensaje no se ha podido enviar.';
                 echo 'Error: ' . $mail->ErrorInfo;
